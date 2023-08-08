c80c3f3b62b52f88c57ccbd90adf1a7c0e788361
VladRaFeed <122728801+VladRaFeed@users.noreply.github.com>
2023-08-08 19:46:00 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,31 +26,31 @@ export const App = () => {
   )
 };
 
-App.propTypes = {
-    user: PropTypes.arrayOf(PropTypes.exact({
-        username: PropTypes.string.isRequired,
-        tag: PropTypes.string.isRequired,
-        location: PropTypes.string.isRequired,
-        avatar: PropTypes.string.isRequired,
-        followers: PropTypes.number.isRequired,
-        views: PropTypes.number.isRequired,
-        likes: PropTypes.number.isRequired,
-    })),
-    data: PropTypes.arrayOf(PropTypes.exact({
-      id: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      percentage: PropTypes.number.isRequired,
-    })),
-    friends: PropTypes.arrayOf(PropTypes.exact({
-      avatar: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      isOnline: PropTypes.bool.isRequired,
-      id: PropTypes.number.isRequired,
-    })),
-    transactions: PropTypes.arrayOf(PropTypes.exact({
-      id: PropTypes.string.isRequired,
-      type: PropTypes.string.isRequired,
-      amount: PropTypes.string.isRequired,
-      currency: PropTypes.string.isRequired,
-    })),
-}
+// App.propTypes = {
+//     user: PropTypes.arrayOf(PropTypes.exact({
+//         username: PropTypes.string.isRequired,
+//         tag: PropTypes.string.isRequired,
+//         location: PropTypes.string.isRequired,
+//         avatar: PropTypes.string.isRequired,
+//         followers: PropTypes.number.isRequired,
+//         views: PropTypes.number.isRequired,
+//         likes: PropTypes.number.isRequired,
+//     })),
+//     data: PropTypes.arrayOf(PropTypes.exact({
+//       id: PropTypes.string.isRequired,
+//       label: PropTypes.string.isRequired,
+//       percentage: PropTypes.number.isRequired,
+//     })),
+//     friends: PropTypes.arrayOf(PropTypes.exact({
+//       avatar: PropTypes.string.isRequired,
+//       name: PropTypes.string.isRequired,
+//       isOnline: PropTypes.bool.isRequired,
+//       id: PropTypes.number.isRequired,
+//     })),
+//     transactions: PropTypes.arrayOf(PropTypes.exact({
+//       id: PropTypes.string.isRequired,
+//       type: PropTypes.string.isRequired,
+//       amount: PropTypes.string.isRequired,
+//       currency: PropTypes.string.isRequired,
+//     })),
+// }
